b15a2b6fae8b80de9238184b7265d7b36b7e0aed
millio <millio@watcha.com>
2021-06-21 16:26:40 +0900

    아이템 이름 상수 위치 변경

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,9 +1,5 @@
 package com.gildedrose
 
-const val CHEESE_NAME ="Aged Brie"
-const val BACKSTAGE_PASS_TICKET_NAME ="Backstage passes to a TAFKAL80ETC concert"
-const val LEGEND_NAME = "Sulfuras, Hand of Ragnaros"
-
 class GildedRose(var items: Array<Item>) {
 
     private val itemSystem : GlidedRoseSystemController = ItemSystem()
--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -1,5 +1,9 @@
 package com.gildedrose
 
+const val CHEESE_NAME ="Aged Brie"
+const val BACKSTAGE_PASS_TICKET_NAME ="Backstage passes to a TAFKAL80ETC concert"
+const val LEGEND_NAME = "Sulfuras, Hand of Ragnaros"
+
 data class Item(
         var name: String,
         var sellIn: Int,
