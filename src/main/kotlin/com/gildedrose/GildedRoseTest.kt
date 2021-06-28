69be43f9142202df23e0abecaf0839b25576c8ac
millio <millio@watcha.com>
2021-06-28 14:24:00 +0900

    ConjuredItem 걸린 경우의 테스트 케이스 작성

--- src/main/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/main/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -4,15 +4,19 @@ import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
+    private val conjured = CONJURED
     private val normalItemName = "Item"
-    private val cheeseItemName = "Aged Brie"
-    private val passTicketName = "Backstage passes to a TAFKAL80ETC concert"
+    private val cheeseItemName = CHEESE_NAME
+    private val passTicketName = BACKSTAGE_PASS_TICKET_NAME
 
-    private val legendItemName = "Sulfuras, Hand of Ragnaros"
+    private val legendItemName = LEGEND_NAME
     private val legendItemQuality = 80
     private val legendItem = Item(legendItemName, 10, legendItemQuality)
 
     private val factory = SystemFactory()
+    private val itemSystem = ItemSystem()
+    private val agedItemSystem = AgedItemSystem()
+    private val ticketSystem = TicketSystem()
 
     @Test
     fun `일반 아이템이면 일반 아이템 시스템에 들어간다`() {
@@ -45,6 +49,10 @@ internal class GildedRoseTest {
         val system = factory.getItemSystem(item)
         assert(system == null)
     }
+
+    /**
+     * 일반 아이템
+     */
 
     @Test
     fun `일반아이템_퀄리티가 0 이상이고 전설템이 아니면 퀄리티가 떨어진다`() {
@@ -81,6 +89,10 @@ internal class GildedRoseTest {
         assert(item.sellIn == -1)
     }
 
+    /**
+     * 치즈 - 에이징되는 아이템
+     */
+
     @Test
     fun `판매일자가 남아있는 치즈 퀄리티는 시간이 지나면 퀄리티가 1 올라간다`() {
         val item = Item(cheeseItemName, 10, 1)
@@ -104,6 +116,10 @@ internal class GildedRoseTest {
         gildedRose.updateQuality()
         assert(item.quality <= 50)
     }
+
+    /**
+     * 티켓 아이템
+     */
 
     @Test
     fun `백스테이지 입장권은 판매 기한 10일 이전까지 퀄리티가 1씩 증가한다`() {
@@ -161,6 +177,9 @@ internal class GildedRoseTest {
         assert(item.quality <= 50)
     }
 
+    /**
+     * 전설 아이템
+     */
 
     @Test
     fun `전설템이면 퀄리티가 떨어지지 않는다`() {
@@ -194,5 +213,179 @@ internal class GildedRoseTest {
         assert(legendItem.sellIn == 10)
     }
 
+    /**
+     *
+     * ConjuredItem 관련 테스트 케이스
+     */
 
+    /**
+     * 마법의 아이템 판별
+     */
+    @Test
+    fun `마법에 걸린 일반 아이템`() {
+        val item = Item(conjured + normalItemName, 10, 10)
+        assert(itemSystem.isConjuredItem(item.name))
+    }
+
+    @Test
+    fun `마법에 걸린 숙성(치즈) 아이템`() {
+        val item = Item(conjured + cheeseItemName, 10, 10)
+        assert(agedItemSystem.isConjuredItem(item.name))
+    }
+
+    @Test
+    fun `마법에 걸린 티켓 아이템`() {
+        val item = Item(conjured + passTicketName, 10, 10)
+        assert(ticketSystem.isConjuredItem(item.name))
+    }
+
+    /**
+     * 마법에 걸린 아이템 시스템 체크
+     */
+
+
+    @Test
+    fun `일반 마법 아이템이면 일반 아이템 시스템에 들어간다`() {
+        val item = Item(conjured+normalItemName, 10, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        val system = factory.getItemSystem(item)
+        assert(system is ItemSystem)
+    }
+
+    @Test
+    fun `마법에 걸린 치즈면 에이징 아이템 시스템에 들어간다`() {
+        val item = Item(conjured+cheeseItemName, 10, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        val system = factory.getItemSystem(item)
+        assert(system is AgedItemSystem)
+    }
+
+    @Test
+    fun `마법에 걸린 백 스테이지 티켓이면 티켓 아이템 시스템에 들어간다`() {
+        val item = Item(conjured+passTicketName, 10, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        val system = factory.getItemSystem(item)
+        assert(system is TicketSystem)
+    }
+
+
+    /**
+     * 일반 마법 아이템
+     */
+    @Test
+    fun `마법 일반아이템_퀄리티가 0 이상이고 전설템이 아니면 퀄리티가 떨어진다`() {
+        val item = Item(conjured + normalItemName, 10, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 8)
+    }
+
+    @Test
+    fun `마법 일반아이템_퀄리티가 0 이상에서 더 떨어질순 없다`() {
+        val item = Item(conjured + normalItemName, 10, 0)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality >= 0)
+    }
+
+    @Test
+    fun `판매일자가 다된 마법 일반아이템 퀄리티가 4배로 떨어진다`() {
+        val item = Item(conjured + normalItemName, 0, 30)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 26)
+
+        gildedRose.updateQuality()
+        assert(item.quality == 22)
+    }
+
+    /**
+     * 에이징 되는 마법 아이템
+     */
+
+    @Test
+    fun `판매일자가 남아있는 마법의 치즈 퀄리티는 시간이 지나면 퀄리티가 2 올라간다`() {
+        val item = Item(conjured + cheeseItemName, 10, 1)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 3)
+    }
+
+    @Test
+    fun `마법의 치즈 퀄리티는 판매일자가 다되고 시간이 가면 퀄리티가 4씩 올라간다`() {
+        val item = Item(conjured + cheeseItemName, 0, 30)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        print("${item.quality}")
+        assert(item.quality == 34)
+    }
+
+    @Test
+    fun `마법의 치즈 퀄리티는 시간이 지나도 퀄리티가 50을 넘을 수 없다`() {
+        val item = Item(conjured + cheeseItemName, 10, 49)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality <= 50)
+    }
+
+    /**
+     * 마법의 티켓 아이템
+     */
+
+    @Test
+    fun `마법의 백스테이지 입장권은 판매 기한 10일 이전까지 퀄리티가 2씩 증가한다`() {
+        val item = Item(conjured + passTicketName, 11, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 12)
+    }
+
+    @Test
+    fun `마법의 백스테이지 입장권은 판매 기한 10일 전이 되면 퀄리티가 4씩 증가한다`() {
+        val item = Item(conjured + passTicketName, 10, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 14)
+    }
+
+    @Test
+    fun `마법의 백스테이지 입장권은 판매 기한 5일전이 되면 퀄리티가 3씩 증가한다`() {
+        val item = Item(conjured + passTicketName, 5, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 16)
+    }
+
+    @Test
+    fun `마법의 백스테이지 입장권은 판매 기한이 다 되면 퀄리티가 0이 된다`() {
+        val item = Item(conjured + passTicketName, 0, 10)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality == 0)
+    }
+
+    @Test
+    fun `마법의 백스테이지 입장권은 시간이 지나도 퀄리티가 50을 넘을 수 없다`() {
+        val item = Item(conjured + passTicketName, 5, 49)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality <= 50)
+    }
+
+
+    @Test
+    fun `마법의 백스테이지 입장권은 시간이 지나도 퀄리티가 50을 넘을 수 없다2`() {
+        val item = Item(conjured + passTicketName, 10, 49)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality <= 50)
+    }
+
+    @Test
+    fun `마법의 백스테이지 입장권은 시간이 지나도 퀄리티가 50을 넘을 수 없다3`() {
+        val item = Item(conjured + passTicketName, 11, 50)
+        val gildedRose = GildedRose(arrayOf(item))
+        gildedRose.updateQuality()
+        assert(item.quality <= 50)
+    }
 }
--- src/main/kotlin/com/gildedrose/SystemFactory.kt
+++ src/main/kotlin/com/gildedrose/SystemFactory.kt
@@ -8,10 +8,10 @@ class SystemFactory {
     private val ticketItemSystem: GlidedRoseSystemController = TicketSystem()
 
     fun getItemSystem(item: Item): GlidedRoseSystemController? {
-        return when (item.name) {
-            CHEESE_NAME -> agedItemSystem
-            BACKSTAGE_PASS_TICKET_NAME -> ticketItemSystem
-            LEGEND_NAME -> null
+        return when  {
+            item.name.contains(CHEESE_NAME) -> agedItemSystem
+            item.name.contains(BACKSTAGE_PASS_TICKET_NAME) -> ticketItemSystem
+            item.name.contains(LEGEND_NAME) -> null
             else -> itemSystem
         }
     }
