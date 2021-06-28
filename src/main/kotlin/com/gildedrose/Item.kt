c7c1d77f0827bad62f4f61a972a95deb23d4a013
millio <millio@watcha.com>
2021-06-28 12:01:35 +0900

    CONJURED 만듬

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -1,5 +1,6 @@
 package com.gildedrose
 
+const val CONJURED = "Conjured"
 const val CHEESE_NAME = "Aged Brie"
 const val BACKSTAGE_PASS_TICKET_NAME = "Backstage passes to a TAFKAL80ETC concert"
 const val LEGEND_NAME = "Sulfuras, Hand of Ragnaros"
